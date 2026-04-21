154de025de350ca31d01d7ca9776cb502009bb1d
paulbardar <Q.C.tulip@gmail.com>
2026-04-21 20:31:24 +0300

    Adapter.h

--- 26_CW_3/Adapter.h
+++ 26_CW_3/Adapter.h
@@ -1,6 +1,20 @@
 #pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
 class Adapter
 {
+	string ID;
+	string interfaceInfo;
+	float price;
+	double speed;
 
+	static int count;
+
+public:
+	Adapter(string interfaceInfo, float price, double speed);
+
+	void shoInfo()const;
 };
 
