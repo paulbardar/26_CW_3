2b29ffec17d0a18491710b8b6c43b7d5e74cd0cd
paulbardar <Q.C.tulip@gmail.com>
2026-04-21 20:43:13 +0300

    main

--- 26_CW_3/26_CW_3.cpp
+++ 26_CW_3/26_CW_3.cpp
@@ -1,10 +1,20 @@
 
 
-#include <iostream>
+#include "Adapter.h"
+#include <vector>
 
-
+int Adapter::count = 1;
 int main()
 {
+    vector<Adapter> admin;
+    admin.push_back(Adapter("usb", 250, 100));
+    admin.push_back(Adapter("usb 3.0", 350, 500));
+    admin.push_back(Adapter("usb/typeC", 150, 150));
+    admin.push_back(Adapter("usb/miniUSB", 175, 175));
+
+    for (auto item = admin.begin(); item != admin.end(); item++) {
+        item->showInfo();
+    }
  
     return 0;
 }
