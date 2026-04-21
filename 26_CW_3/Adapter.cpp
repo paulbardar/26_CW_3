94d1fc6c28e81f1c31d80132e0f9b382ba3197e0
paulbardar <Q.C.tulip@gmail.com>
2026-04-21 20:36:59 +0300

    Adapter.cpp

--- 26_CW_3/Adapter.cpp
+++ 26_CW_3/Adapter.cpp
@@ -2,9 +2,19 @@
 
 Adapter::Adapter(string interfaceInfo, float price, double speed)
 {
+	this->interfaceInfo = interfaceInfo;
+	this->price = price;
+	this->speed = speed;
 
+	ID = "WFID" + to_string(count / 100) + to_string(count / 10 % 10) + to_string(count % 10);
+	count++;
 }
 
-void Adapter::shoInfo() const
+void Adapter::showInfo() const
 {
+	cout << "ID: " << ID << endl;
+	cout << "Interface: " << interfaceInfo << endl;
+	cout << "Speed: " << speed << endl;
+	cout << "Price: " << price << endl;
+
 }
